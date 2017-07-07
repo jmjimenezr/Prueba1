094bffd2e6d7c43d0cfe3c2c481ab1bb177ebab7
jmjimenezr <juan.199009o@gmail.com>
2017-07-07 18:57:35 -0500

    Initial commit

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Prueba1.Models;
+
+namespace Prueba1.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private TestEntities db = new TestEntities();
+        
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Usuarios.ToListAsync());
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "IdUsuario,Nombre,ApellidoP,ApellidoM,Telefono,Direccion")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuarios);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuarios);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
